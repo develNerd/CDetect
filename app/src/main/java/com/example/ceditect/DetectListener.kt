b4a35b00d1a3fdf5e8ccde065842af1a5ac024df
Ghoster1 <isaacakakpo4@gmail.com>
2020-04-28 23:36:06 +0000

    Intro screen

--- app/src/main/java/com/example/ceditect/DetectListener.kt
+++
@@ -1,8 +0,0 @@
-package com.example.ceditect
-
-interface DetectListener {
-    fun onStarted()
-    fun onSuccess(labelValue:String)
-    fun onFailure(error:String)
-    fun onModelDownloadComplete(successMessage:String)
-}
